a9fa6b1a6065c04ed9629dc53bbcfb068bd1557e
Ritesh Pokale <riteshspoklae27@gmail.com>
2025-09-08 15:17:48 +0530

    final commit 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-600">
-        🚀 React + Vite + Tailwind App is Running!
+        React + Vite + Tailwind App is Running!
       </h1>
     </div>
   );
